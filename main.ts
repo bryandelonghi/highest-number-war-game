8157deec05b845fa4efd1d14f7271ca7ad8ecf0a
bryandelonghi <113536458+bryandelonghi@users.noreply.github.com>
2022-10-28 11:28:18 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.A, function () {
+    number = randint(1, 10)
+    basic.showNumber(number)
+})
+input.onButtonPressed(Button.B, function () {
+    if (true) {
+        wins += 1
+        basic.showNumber(wins)
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+})
+let number = 0
+let wins = 0
+wins = 0
