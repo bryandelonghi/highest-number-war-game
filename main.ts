e5729575b5c1cdd1d60479e78ddd1b03aba838eb
bryandelonghi <113536458+bryandelonghi@users.noreply.github.com>
2022-11-15 14:30:40 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,6 +2,9 @@ input.onButtonPressed(Button.A, function () {
     number = randint(1, 10)
     basic.showNumber(number)
 })
+input.onButtonPressed(Button.AB, function () {
+    wins = 0
+})
 input.onButtonPressed(Button.B, function () {
     if (true) {
         wins += 1
